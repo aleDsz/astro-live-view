7f91c35f3d6c191a6bb3465935629f302bb01c67
Alexandre de Souza <alexandre@aledsz.com.br>
2020-11-29 22:30:19 +0000

    Add Astro supported values

--- lib/astro.ex
+++ lib/astro.ex
@@ -1,18 +1,113 @@
 defmodule Astro do
   @moduledoc """
-  Documentation for Astro.
+  Astro is a design system developed by Magnetis.
+
+  This library provides LiveView components to use
+  together with [@magnetis/astro](https://www.npmjs.com/package/@magnetis/astro) npm package.
   """
+
+  @typep colors ::
+           :earth
+           | :venus
+           | :uranus
+           | :moon
+           | :mars
+           | :sun
+           | :space
+
+  @typep gradients ::
+           :nebulosa
+           | :andromeda
+           | :sombrero
+           | :milky_way
+           | :hoag
+           | :centaurus
+           | :whirlpool
+           | :cartwheel
+           | :black_hole
+           | :mayall
+           | :pinwheel
+
+  @typep sizes :: :small | :medium | :large
+
+  @colors ~w(
+    earth
+    venus
+    uranus
+    moon
+    mars
+    sun
+    space
+  )a
+  defguard is_color(color) when color in @colors
+
+  @doc "Gets available colors"
+  @spec colors() :: list(colors())
+  def colors, do: @colors
+
+  @gradients ~w(
+    nebulosa
+    andromeda
+    sombrero
+    milky_way
+    hoag
+    centaurus
+    whirlpool
+    cartwheel
+    black_hole
+    mayall
+    pinwheel
+  )a
+  defguard is_gradient(gradient) when gradient in @gradients
+
+  @doc "Gets available gradients"
+  @spec gradients() :: list(gradients())
+  def gradients, do: @gradients
+
+  @sizes ~w(small medium large)a
+  defguard is_size(size) when size in @sizes
+
+  @doc "Gets available sizes"
+  @spec sizes() :: list(sizes())
+  def sizes, do: @sizes
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Astro.hello()
-      :world
-
+  Gets color class name
   """
-  def hello do
-    :world
+  @spec color(colors()) :: binary()
+  def color(color_name) when is_color(color_name) do
+    normalize_class(color_name)
+  end
+
+  def color(value), do: raise(Astro.Error, type: :color, value: value)
+
+  @doc """
+  Gets gradient color class name
+  """
+  @spec gradient(gradients()) :: binary()
+  def gradient(gradient_name) when is_gradient(gradient_name) do
+    normalize_class(gradient_name)
+  end
+
+  def gradient(value), do: raise(Astro.Error, type: :gradient, value: value)
+
+  @doc """
+  Gets size class name
+  """
+  @spec size(sizes()) :: binary()
+  def size(size_name) when is_size(size_name) do
+    normalize_class(size_name)
+  end
+
+  def size(value), do: raise(Astro.Error, type: :size, value: value)
+
+  @doc """
+  Normalizes atom into binary class name
+  """
+  @spec normalize_class(atom()) :: binary()
+  def normalize_class(atom) do
+    atom
+    |> Atom.to_string()
+    |> String.replace(~r/[_]+/, "-")
   end
 end
--- test/astro_test.exs
+++ test/astro_test.exs
@@ -1,8 +1,98 @@
 defmodule AstroTest do
-  use ExUnit.Case
-  doctest Astro
+  use Astro.Case
 
-  test "greets the world" do
-    assert Astro.hello() == :world
+  describe "guards" do
+    defmodule GuardTest do
+      require Astro
+
+      def is_color(value), do: Astro.is_color(value)
+      def is_gradient(value), do: Astro.is_gradient(value)
+      def is_size(value), do: Astro.is_size(value)
+    end
+
+    test "is_color/1 should validate color" do
+      assert GuardTest.is_color(:venus)
+      refute GuardTest.is_color(:foobar)
+    end
+
+    test "is_gradient/1 should validate gradient" do
+      assert GuardTest.is_gradient(:black_hole)
+      refute GuardTest.is_gradient(:foobar)
+    end
+
+    test "is_size/1 should validate size" do
+      assert GuardTest.is_size(:small)
+      refute GuardTest.is_size(:foobar)
+    end
+  end
+
+  describe "colors/0" do
+    test "returns a list of available colors" do
+      assert colors = module().colors()
+      assert length(colors) == 7
+      assert :venus in colors
+    end
+  end
+
+  describe "gradients/0" do
+    test "returns a list of available gradients" do
+      assert gradients = module().gradients()
+      assert length(gradients) == 11
+      assert :black_hole in gradients
+    end
+  end
+
+  describe "sizes/0" do
+    test "returns a list of available sizes" do
+      assert [:small, :medium, :large] = module().sizes()
+    end
+  end
+
+  describe "color/1" do
+    test "returns a color class from given color atom" do
+      assert "venus" == module().color(:venus)
+    end
+
+    test "returns error when given color atom is invalid" do
+      assert_raise Astro.Error, fn -> module().color(:foobar) end
+    end
+  end
+
+  describe "gradient/1" do
+    test "returns a gradient class from given gradient atom" do
+      assert "andromeda" == module().gradient(:andromeda)
+    end
+
+    test "returns a gradient class from given underscored gradient atom" do
+      assert "black-hole" == module().gradient(:black_hole)
+    end
+
+    test "returns error when given gradient atom is invalid" do
+      assert_raise Astro.Error, fn -> module().gradient(:foobar) end
+    end
+  end
+
+  describe "size/1" do
+    test "returns a size class from given size atom" do
+      assert "small" == module().size(:small)
+    end
+
+    test "returns error when given size atom is invalid" do
+      assert_raise Astro.Error, fn -> module().size(:foobar) end
+    end
+  end
+
+  describe "normalize_class/1" do
+    test "returns a normalized class from given atom" do
+      assert "foobar" == module().normalize_class(:foobar)
+    end
+
+    test "returns a normalized class from given underscored atom" do
+      assert "foobar-test" == module().normalize_class(:foobar_test)
+    end
+
+    test "returns error when given argument isn't atom" do
+      assert_raise ArgumentError, fn -> module().normalize_class("foobar") end
+    end
   end
 end
