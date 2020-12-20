69dfefbfc4a4cb7020980ed39f029f25c1fb0b52
Alexandre de Souza <alexandre@aledsz.com.br>
2020-12-20 10:06:15 -0300

    Add OutlineButton component

---
+++ lib/astro/surface_components/outline_button.ex
@@ -0,0 +1,35 @@
+defmodule Astro.SurfaceComponents.OutlineButton do
+  @moduledoc """
+  Astro outline button.
+
+  For more information, see Storybook: https://astro.magnetis.com.br/buttons#outline-buttons
+  """
+  use Astro.SurfaceComponent
+
+  @overridable_value_for_props [
+    color: "outline"
+  ]
+  prefix("a-btn")
+
+  @doc """
+  Button background and foreground color
+  """
+  prop color, :atom, values: Astro.colors()
+
+  @doc """
+  Button background and foreground gradient color
+  """
+  prop gradient, :atom, values: Astro.gradients()
+
+  @doc """
+  Button size
+  """
+  prop size, :atom, values: Astro.sizes()
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <button disabled={{ @isDisabled }} class="{{ generate_class_names(assigns) }}"><slot/></button>
+    """
+  end
+end
---
+++ test/astro/surface_components/outline_button_test.exs
@@ -0,0 +1,91 @@
+defmodule Astro.SurfaceComponents.OutlineButtonTest do
+  use Astro.ComponentCase
+
+  describe "render/1" do
+    test "returns OutlineButton component without button properties" do
+      assigns = %{}
+
+      html =
+        render_surface do
+          ~H"""
+          <OutlineButton id="sample_button">Sample</OutlineButton>
+          """
+        end
+
+      assert html =~ """
+             <button class="a-btn">Sample</button>
+             """
+    end
+
+    test "returns OutlineButton component with color property" do
+      assigns = %{
+        color: :venus
+      }
+
+      html =
+        render_surface do
+          ~H"""
+          <OutlineButton id="sample_button" color={{ @color }}>Sample</OutlineButton>
+          """
+        end
+
+      assert html =~ """
+             <button class="a-btn a-btn--outline-venus">Sample</button>
+             """
+    end
+
+    test "returns OutlineButton component with size property" do
+      assigns = %{
+        size: :large
+      }
+
+      html =
+        render_surface do
+          ~H"""
+          <OutlineButton id="sample_button" size={{ @size }}>Sample</OutlineButton>
+          """
+        end
+
+      assert html =~ """
+             <button class="a-btn a-btn--large">Sample</button>
+             """
+    end
+
+    test "returns OutlineButton component with color and size properties" do
+      assigns = %{
+        size: :large,
+        color: :earth
+      }
+
+      html =
+        render_surface do
+          ~H"""
+          <OutlineButton id="sample_button" size={{ @size }} color={{ @color }}>Sample</OutlineButton>
+          """
+        end
+
+      assert html =~ """
+             <button class="a-btn a-btn--large a-btn--outline-earth">Sample</button>
+             """
+    end
+
+    test "returns disabled OutlineButton component with color and size properties" do
+      assigns = %{
+        isDisabled: true,
+        size: :large,
+        color: :earth
+      }
+
+      html =
+        render_surface do
+          ~H"""
+          <OutlineButton id="sample_button" isDisabled={{ @isDisabled }} size={{ @size }} color={{ @color }}>Sample</OutlineButton>
+          """
+        end
+
+      assert html =~ """
+             <button disabled class="a-btn a-btn--large a-btn--outline-earth">Sample</button>
+             """
+    end
+  end
+end
